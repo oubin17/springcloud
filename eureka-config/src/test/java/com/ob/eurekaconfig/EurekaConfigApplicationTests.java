a352d0356c6e8a1baaab9de8ae7f81a9073558cd
欧斌 <171913@nd.com>
2020-06-11 17:45:39 +0800

    spring cloud config

---
+++ eureka-config/src/test/java/com/ob/eurekaconfig/EurekaConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ob.eurekaconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
