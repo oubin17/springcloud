30197edb8379493c8621db76cefd7adf754db66c
欧斌 <171913@nd.com>
2020-06-12 14:10:51 +0800

    eureka config client

---
+++ eureka-config-client/src/test/java/com/ob/eurekaconfigclient/EurekaConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ob.eurekaconfigclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
