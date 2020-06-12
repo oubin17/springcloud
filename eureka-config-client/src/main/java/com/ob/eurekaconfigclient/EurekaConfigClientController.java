1da3f032888418b629e82a9a6aa963b47ede4200
欧斌 <171913@nd.com>
2020-06-12 15:35:58 +0800

    eureka config:高可用分布式配置中心

---
+++ eureka-config-client/src/main/java/com/ob/eurekaconfigclient/EurekaConfigClientController.java
@@ -0,0 +1,25 @@
+package com.ob.eurekaconfigclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: oubin
+ * @Date: 2020/6/12 14:24
+ * @Description:
+ */
+@RefreshScope
+@RestController
+@RequestMapping("/v1.0")
+public class EurekaConfigClientController {
+
+//    @Value("${info.profile}")
+//    private String profile;
+//
+//    @RequestMapping("/config/client")
+//    public String from() {
+//        return this.profile;
+//    }
+}
