13dc552b6902f7ced67311fc3e4a2f50cc43ed14
欧斌 <171913@nd.com>
2020-06-17 11:32:44 +0800

    create config-repo

--- eureka-config-client/src/main/java/com/ob/eurekaconfigclient/EurekaConfigClientApplication.java
+++ eureka-config-client/src/main/java/com/ob/eurekaconfigclient/EurekaConfigClientApplication.java
@@ -8,6 +8,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class EurekaConfigClientApplication {
 
+    /**
+     * 通过config服务端获取配置文件的时候，远端需要存在对应的文件：name=eureka-config-client, profiles=[dev], label=master
+     * 否则获取失败
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(EurekaConfigClientApplication.class, args);
     }
