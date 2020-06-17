6b62e1176d4eacd1f98d7a722820b70672f969e8
欧斌 <171913@nd.com>
2020-06-17 16:50:00 +0800

    stream RabbitMQ

---
+++ stream-rabbitmq/src/main/java/com/ob/streamrabbitmq/SinkReceiver.java
@@ -0,0 +1,23 @@
+package com.ob.streamrabbitmq;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+/**
+ * @Author: oubin
+ * @Date: 2020/6/17 14:48
+ * @Description:
+ */
+@EnableBinding(Sink.class)
+public class SinkReceiver {
+
+    private static Logger logger = LoggerFactory.getLogger(SinkReceiver.class);
+
+    @StreamListener(Sink.INPUT)
+    public void receive(Object payload) {
+        logger.info("RECEIVED: " + payload);
+    }
+}
---
+++ stream-rabbitmq/src/test/java/com/ob/streamrabbitmq/StreamRabbitmqApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ob.streamrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = StreamRabbitmqApplication.class)
+class StreamRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
