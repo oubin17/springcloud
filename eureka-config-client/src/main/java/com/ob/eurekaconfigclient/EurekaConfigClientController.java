74c5fade777633bad1db93baffcc9b0c64fb1914
欧斌 <171913@nd.com>
2020-06-18 16:22:16 +0800

    bus 消息总线

--- eureka-config-client/src/main/java/com/ob/eurekaconfigclient/EurekaConfigClientController.java
+++ eureka-config-client/src/main/java/com/ob/eurekaconfigclient/EurekaConfigClientController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @Author: oubin
  * @Date: 2020/6/12 14:24
- * @Description:
+ * @Description: 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
  */
 @RefreshScope
 @RestController
